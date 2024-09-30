3e1a770ce6a8f41f20141bee0a00bcae62f2138e
Venkat K <venkatkommina91@gmail.com>
2024-09-30 12:28:51 +0530

    init

---
+++ node-app/src/a.ts
@@ -0,0 +1,78 @@
+// let x: number = 10;
+// console.log(x);
+
+function greet(firstName: string) {
+  console.log(`Hello ${firstName}`);
+}
+
+greet("John");
+
+//type inference
+function sum(a: number, b: number) {
+  return a + b;
+}
+
+//string, number, boolean, any**, void
+
+console.log(sum(2, 8));
+
+function ageCheck(age: number): boolean {
+  if (age > 17) {
+    return true;
+  }
+  return false;
+}
+
+let x = ageCheck(20); //type inference->no need to specify the type explicitly
+
+console.log(ageCheck(20));
+console.log(ageCheck(5));
+console.log(ageCheck(17));
+console.log(ageCheck(17.5));
+
+function returnObj(name: string, age: number): { name: string; age: number } {
+  //sometimes they are unavoidable or what? No, typescript infers to what we are returning right! If we explicitly specify, we get an error
+  return {
+    name: name,
+    age: age,
+  };
+}
+
+let obj = returnObj("John", 20);
+console.log(obj);
+
+function runAfter1s(functionToRun: () => void) {
+  setTimeout(functionToRun, 1000);
+}
+//Thing to learn: giving types to the functions
+
+//learning some important ts config stuff
+
+//Chat
+//We can use zod for type in javascript, is there any difference?
+//zod does type checks during runtime(99% of the time), while this is during the compile time.
+
+//interfaces:
+
+interface User {
+  name: string;
+  age: number;
+}
+
+// 1st usecase: it is giving types to like what we've seen for objects
+function greetUser(user: User) {
+  console.log(`Hi ${user.name}`);
+}
+
+function isAdult(user: User): boolean {
+  if (user.age > 17) {
+    return true;
+  }
+  return false;
+}
+
+let isAdultTrue = isAdult({ name: "John", age: 20 });
+console.log(isAdultTrue);
+greetUser({ name: "John", age: 20 });
+
+//implementing interfaces, popular interview question: what is the difference between interfaces and types?
